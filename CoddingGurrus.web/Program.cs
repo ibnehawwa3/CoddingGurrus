01a53c51ff65c17fec63cfa14cfc81ed85f112e8
nasirkiyani360 <nasirkiyani360@gmail.com>
2024-01-22 22:59:00 +0500

    default route changed from home/index to account/register.

--- CoddingGurrus.web/Program.cs
+++ CoddingGurrus.web/Program.cs
@@ -18,6 +18,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    //pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Register}/{id?}");
 
 app.Run();
