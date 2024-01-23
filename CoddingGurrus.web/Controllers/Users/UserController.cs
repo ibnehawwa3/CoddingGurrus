09073a7c8c2c5de9dbe6aebd9e147ae3ab78917d
nasirkiyani360 <nasirkiyani360@gmail.com>
2024-01-24 00:30:12 +0500

    Dummy user list added.

---
+++ CoddingGurrus.web/Controllers/Users/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoddingGurrus.web.Controllers.Users
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
